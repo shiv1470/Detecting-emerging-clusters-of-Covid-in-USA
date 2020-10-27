3fe6bd631289d128d880f6bb9e1819ba1bd91d2b
Shivam Gupta <gshivamgupta10@gmail.com>
2020-10-27 20:01:38 +0530

    converted text file to json

--- texttojson.py
+++ texttojson.py
@@ -1,17 +1,18 @@
 import json
 data={}    
-data['circle'] = []
+cnt=0
 with open("hotspots_satscan.txt","r") as inputFile:
     for line in inputFile:
+        cnt+=1
         s=line.split()
         centre=s[4].split(",")
         centre[0]=centre[0][1:]
-        centre[1]=centre[1][:len(centre[1])-1]
-        data["circle"].append({
+        centre[1]=centre[1][:len(centre[1])-2]
+        data["circle"+str(cnt)]={
             "centre" : centre,
-            "radius" : s[8][:len(s[8])-1],
-            "LogLRZ" : s[11][:len(s[11])-1]
-            })
+            "radius" : s[10][:len(s[10])-1],
+            "LogLRZ" : s[13][:len(s[13])-1]
+            }
 
 with open('hotspots_satscan.json', 'w') as outfile:
     json.dump(data, outfile)
