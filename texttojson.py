63e03ee759adbf638231ae119e33f9310d241a4a
Shivam Gupta <gshivamgupta10@gmail.com>
2020-10-27 19:56:46 +0530

    converted text file to json

--- texttojson.py
+++ texttojson.py
@@ -6,7 +6,7 @@ with open("hotspots_satscan.txt","r") as inputFile:
         s=line.split()
         centre=s[4].split(",")
         centre[0]=centre[0][1:]
-        centre[1]=centre[1][len(centre[1])-1]
+        centre[1]=centre[1][:len(centre[1])-1]
         data["circle"].append({
             "centre" : centre,
             "radius" : s[8][:len(s[8])-1],
